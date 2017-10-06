95eabfaf03e7bb50c6c404999b26aec377509944
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    rook (Red3) pass test

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,23 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
 
+    // Rook can move any number of spaces horizontally or vertically. 
+    // (1,2) = 1st row, 2nd column (starting from 0 to 7)
+
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        const moves = []
+        for (let i = 0; i <= 7; i++) {
+        if (location.col !==i) moves.push(Square.at(location.row, i))
+            }   
+        for (let i = 0; i <= 7; i++) {
+        if (location.row !==i) moves.push(Square.at(i, location.col)) 
+        }
+        return moves
     }
 }
