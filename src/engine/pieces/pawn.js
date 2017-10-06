514d46267d580ed5ff6b6e948ad30ac0bcee8a17
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    Red18 Add end of board rule

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -14,19 +14,24 @@ export default class Pawn extends Piece {
             if (location.row === 1) {
                 if (board.getPiece(Square.at(location.row + 2, location.col)) === undefined && board.getPiece(Square.at(location.row + 1, location.col)) === undefined) {
                     moves.push(Square.at(location.row + 2, location.col))
-                }}
+                }
+            }
+            if (location.row !== 7) {
                 if (board.getPiece(Square.at(location.row + 1, location.col)) === undefined) {
                     moves.push(Square.at(location.row + 1, location.col))
                 }
-            
+            }
+
         } else {
             if (location.row === 6) {
                 if (board.getPiece(Square.at(location.row - 2, location.col)) === undefined && board.getPiece(Square.at(location.row - 1, location.col)) === undefined) {
                     moves.push(Square.at(location.row - 2, location.col))
                 }
             }
-            if (board.getPiece(Square.at(location.row - 1, location.col)) === undefined) {
-                moves.push(Square.at(location.row - 1, location.col))
+            if (location.row !== 0) {
+                if (board.getPiece(Square.at(location.row - 1, location.col)) === undefined) {
+                    moves.push(Square.at(location.row - 1, location.col))
+                }
             }
         }
         return moves
