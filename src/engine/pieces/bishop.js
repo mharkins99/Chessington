8891cd4e94d1e297d50ca4555b9ba266a820074f
Michael <minimike100@gmail.com>
2023-01-24 14:16:18 +0000

    Red4 Bishop valid moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,74 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        const moves = []
+        let rowCountRight = location.row + 1;
+        let rowCountLeft = location.row - 1;
+        for (let i = location.col + 1; i <= 7; i++) {
+            if (board.getPiece(Square.at(rowCountRight, i)) === undefined) {
+                    moves.push(Square.at(rowCountRight, i))
+                    rowCountRight++
+                    if (rowCountRight === 8) { break }
+            
+            }
+            else if (board.getPiece(Square.at(rowCountRight, i)) instanceof King === true) { break }
+            else if (board.getPiece(Square.at(rowCountRight, i)).player === Player.BLACK) {
+                moves.push(Square.at(rowCountRight, i))
+                break
+            } else {
+                break
+            }
+        }
+         rowCountRight = location.row + 1;
+         rowCountLeft = location.row - 1;
+        for (let i = location.col + 1; i <= 7; i++) {
+            if (board.getPiece(Square.at(rowCountLeft, i)) === undefined) {
+                    moves.push(Square.at(rowCountLeft, i))
+                    rowCountLeft--
+                    if (rowCountLeft === -1) { break }
+                }
+            
+            else if (board.getPiece(Square.at(rowCountLeft, i)) instanceof King === true) { break }
+            else if (board.getPiece(Square.at(rowCountLeft, i)).player === Player.BLACK) {
+                moves.push(Square.at(rowCountLeft, i))
+                break
+            } else {
+                break
+            }
+        }
+        rowCountRight = location.row + 1;
+        rowCountLeft = location.row - 1;
+        for (let i = location.col - 1; i >= 0; i--) {
+            if (board.getPiece(Square.at(rowCountRight, i)) === undefined) {
+                    moves.push(Square.at(rowCountRight, i))
+                    rowCountRight++
+                    if (rowCountRight === 8) { break }
+                }
+            else if (board.getPiece(Square.at(rowCountRight, i)) instanceof King === true) { break }
+            else if (board.getPiece(Square.at(rowCountRight, i)).player === Player.BLACK) {
+                moves.push(Square.at(rowCountRight, i))
+                break
+            } else {
+                break
+            }
+        }
+        rowCountRight = location.row + 1;
+        rowCountLeft = location.row - 1;
+        for (let i = location.col - 1; i >= 0; i--) {
+            if (board.getPiece(Square.at(rowCountLeft, i)) === undefined) {
+                    moves.push(Square.at(rowCountLeft, i))
+                    rowCountLeft--
+                    if (rowCountLeft === -1) { break }
+            }
+            else if (board.getPiece(Square.at(rowCountLeft, i)) instanceof King === true) { break }
+            else if (board.getPiece(Square.at(rowCountLeft, i)).player === Player.BLACK) {
+                moves.push(Square.at(rowCountLeft, i))
+                break
+            } else {
+                break
+            }
+        }
+        return moves;
     }
 }
