2d7586ebc7e925feb6cb930dbeaf51538ca74db6
Michael <minimike100@gmail.com>
2023-01-24 15:18:51 +0000

    Knight rules met

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,8 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
+import King from './king';
+
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +10,25 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        const moves = []
+        function knightMoves(row, col) {
+            if (board.getPiece(Square.at(row, col)) === undefined) {
+                moves.push(Square.at(row, col))
+            }
+            else if (board.getPiece(Square.at(row, col)).player === Player.BLACK) {
+                moves.push(Square.at(row, col))
+            }
+        };
+        knightMoves(location.row + 1, location.col - 2);
+        knightMoves(location.row + 1, location.col + 2);
+        knightMoves(location.row - 1, location.col + 2);
+        knightMoves(location.row - 1, location.col - 2);
+        knightMoves(location.row + 2, location.col + 1);
+        knightMoves(location.row + 2, location.col - 1);
+        knightMoves(location.row - 2, location.col + 1);
+        knightMoves(location.row - 2, location.col - 1);
+
+        return moves
     }
 }
